477dde08ee0ab732a3624b695c716340e067726c
Jecolia <jecoliatieleo@gmail.com>
2024-12-16 02:45:05 -0800

    all validate

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,4 +2,26 @@
 """utf -8"""
 
 
-def validUTF
+def validUTF8(data):
+    """validation of utf8"""
+    num_byte = 0
+
+    for i in data:
+        byte = i & 0xFF
+
+        if num_byte == 0:
+            if (byte >> 7) == 0b0:
+                continue
+            elif (byte >> 5) == 0b110:
+                num_byte = 1
+            elif (byte >> 4) == 0b1110:
+                num_byte = 2
+            elif (byte >> 3) == 0b11110:
+                num_byte = 3
+            else:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_byte = -1
+    return num_byte == 0
